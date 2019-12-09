bd3d4f698c31321d2971c6377dc00812b67f631b
Shubham Agnihotri <shubham.agnihotri1997@gmail.com>
2019-12-09 20:51:23 +0530

    update

--- Session_19_DataAnnotation/README.md
+++ Session_19_DataAnnotation/README.md
@@ -5,3 +5,20 @@ The dataset contains the images of people looking in some direction which is Up,
 
 After Labelling, the centroid of bounding box with and without location is extracted and 4 potencial Anchor Boxes are generated using KMeans Algorithm.
 
+
+rename_resize.py is the script to rename and resize the images to 400 x 400.
+
+kmeans_with_location.py is used to find the potencial anchor boxes. 
+
+Here, I have calculated k means wth and and without including initial location.
+
+
+![Kmeans Without Initial Location](Assets/kmeans.png)
+
+Here the four potencial bounding boxes are decided based just over the size of the box
+
+![Kmeans With Initial Location](Assets/kmeans_with_location.png)
+
+Here the four potencial bounding boxes are decided based over the size of the box and location of the boxes.
+
+[Link to Dataset](https://drive.google.com/drive/folders/1IXQHnZ2I55tlWahcsfY4gIIoJtUcXN3t)
---
+++ Session_19_DataAnnotation/kmeans_with_location.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+csv = pd.read_csv('via_export_csv.csv')
+# print(csv)
+centroid = []
+width = 0
+height = 0
+for index, row in csv.iterrows():
+	x = row['region_shape_attributes']
+	x = x.split(',')
+	for i in range(len(x)):
+		#print(i)
+		if i == 0:
+			y = x[i].split(':')
+			height = int(y[1])/2
+		if i == 1:
+			y = x[i].split(':')
+			width = int(y[1])/2
+		if i==2:
+			y = x[i].split(':')
+			height =height + int(y[1])/2
+		if i ==4:
+			y = x[i].split(':')
+			y = y[1][:-1]
+			width = width + int(y)/2
+	# print(x[0])
+		# print(width, height)
+	value = [width,height]
+	centroid.append(value)
+	
+# print(centroid)
+
+from sklearn.cluster import KMeans
+import numpy as np
+
+X = np.array(centroid)
+kmeans = KMeans(n_clusters=4, random_state=0).fit(X)
+print(kmeans.labels_)
+print(kmeans.cluster_centers_)
+
+
+
+plt.scatter(X[:, 0], X[:, 1], c=kmeans.labels_, s=50, cmap='viridis_r')
+
+centers = kmeans.cluster_centers_
+plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
+#plt.show()
+
+plt.savefig('kmeans_with_location.png', dpi=1200)
+
